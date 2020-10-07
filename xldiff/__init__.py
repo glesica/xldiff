340e1f6b52343f271e02d5929be9dbedbb2e6a5d
George Lesica <george@lesica.com>
2020-10-06 20:49:02 -0600

    Clean up installation

--- xldiff/__init__.py
+++ xldiff/__init__.py
@@ -4,3 +4,11 @@ format (.xlsx).
 """
 
 __version__ = "0.1.0"
+
+import sys
+
+from xldiff.app import diff_files
+
+
+def main():
+    diff_files(sys.argv[1], sys.argv[2])
--- xldiff/__main__.py
+++ xldiff/__main__.py
@@ -1,28 +1,5 @@
-import sys
-
-from openpyxl import load_workbook
-
-from xldiff.diff import first_difference, print_row
-
-
-def main(left_filename, right_filename):
-    left = load_workbook(left_filename, data_only=True)
-    right = load_workbook(right_filename, data_only=True)
-    diff, index = first_difference(left.active, right.active)
-
-    if diff is None:
-        sys.exit(0)
-
-    print(f"Difference at row {index}")
-
-    print("⬅ ", end="")
-    print_row(diff[0])
-
-    print("➡ ", end="")
-    print_row(diff[1])
-
-    sys.exit(1)
+from xldiff import main
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    main()
